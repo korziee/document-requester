bd124ac5e458e5e0b13c701d5c3e6b846ade9e14
Kory Porter <kory.porter@eucalyptus.vc>
2023-01-26 07:02:45 +1100

    router for many docs

--- src/router.ts
+++ src/router.ts
@@ -10,5 +10,35 @@ router.get("/", async (req, env: Env, ctx: ExecutionContext) => {
   return new Response("ok");
 });
 
+router.post(
+  "/request/:documentId",
+  async (req, env: Env, ctx: ExecutionContext) => {
+    console.log(req);
+    console.log(env);
+    console.log(ctx);
+    return new Response("ok");
+  }
+);
+
+router.put(
+  "/accept/:requestId",
+  async (req, env: Env, ctx: ExecutionContext) => {
+    console.log(req);
+    console.log(env);
+    console.log(ctx);
+    return new Response("ok");
+  }
+);
+
+router.put(
+  "/reject/:requestId",
+  async (req, env: Env, ctx: ExecutionContext) => {
+    console.log(req);
+    console.log(env);
+    console.log(ctx);
+    return new Response("ok");
+  }
+);
+
 // .all is default handler, so 404 for everything else
 router.all("*", () => new Response("Not Found.", { status: 404 }));
